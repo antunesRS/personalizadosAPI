2dbd3ef7d7af48b001d77a2c5fa4463659e66eec
antunesRS <antunesrd@gmail.com>
2021-06-26 14:57:57 -0300

    commit inicial

---
+++ src/main/java/com/br/personalizados/PersonalizadosApiApplication.java
@@ -0,0 +1,13 @@
+package com.br.personalizados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalizadosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalizadosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/personalizados/PersonalizadosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.personalizados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalizadosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
